725ba64faaa2dcc57ed9a0d8ac19ee188013dd2d
Hasith Udayanga <uhasith5@gmail.com>
2024-07-11 14:13:55 +0530

    tiktok test endpoint

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const connectDb = require("./config/dbConnection");
 const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
 
-connectDb();
+// connectDb();
 const app = express();
 
 const port = process.env.PORT || 5000;
